c5fbbf417913189eac78cf280cf1feaa7277e3ad
hyltonjean <hyltonjean@gmail.com>
2020-04-24 21:42:32 +0200

    Completed building todo-app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app" class="container">
+		<img class="logo" alt="Vue logo" src="./assets/logo.png" />
+		<todo-list />
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from "./components/TodoList.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	components: {
+		TodoList,
+	},
+	data() {
+		return {};
+	},
+};
 </script>
 
 <style>
+* {
+	box-sizing: border-box;
+}
+
+.container {
+	max-width: 600px;
+	margin: 0 auto;
+}
+
+.logo {
+	display: block;
+	margin: 20px auto;
+	height: 75px;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	/* text-align: center; */
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 24px;
 }
 </style>
